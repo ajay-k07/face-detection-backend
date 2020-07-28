293e0433874a04d04959055faccf7a887aebd682
ajai lee <ajaidonglee@gmail.com>
2020-07-28 17:08:47 +0530

    lee

--- controllers/Register.js
+++ controllers/Register.js
@@ -26,7 +26,7 @@ const handelRegister = (req, res, db, bcrypt) => {
       .then(trx.commit)
       .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register'))
+    .catch(err => res.status(400).json('database problem'))
 }
 
 module.exports = {
