245af2d02ddd47ed06074d1a7d5a9ca9e1ea7c69
ajai lee <ajaidonglee@gmail.com>
2020-07-28 18:38:24 +0530

    lee

--- controllers/Register.js
+++ controllers/Register.js
@@ -3,7 +3,7 @@ const handleRegister = (req, res, db, bcrypt) => {
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
-  const hash = bcrypt.hashSync(password);
+ const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
       trx.insert({
         hash: hash,
