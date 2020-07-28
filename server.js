844b597383fd649daa8054033e88cfdbc0ac55c8
ajai lee <ajaidonglee@gmail.com>
2020-07-28 17:44:20 +0530

    done

--- server.js
+++ server.js
@@ -18,9 +18,10 @@ const profile=require('./controllers/profile.js');
 const db = knex({
 	client:'pg',
 	connection:{
-		connectionString: process.env.DATABASE_URL,
-		ssl:true,
-	}
+	connectionString: process.env.DATABASE_URL,
+ 	ssl: {
+    	rejectUnauthorized: false
+    }
 });
 
 
