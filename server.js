6177b3758ecd1a9e5cea6cff5736888d044839fc
ajai lee <ajaidonglee@gmail.com>
2020-07-28 14:11:34 +0530

    heroku

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ app.put('/image' ,(req,res)=>{image.handelimage(req,res,db)})
 app.post('/imageurl' ,(req,res)=>{image.handeApicall(req,res)})
 //PORT TO LISTEN
 
-app.listen(3000,()=>{
-	console.log('app is running on port 3000')
+app.listen(process.env.PORT||3000,()=>{
+	console.log(`app is running on port ${process.env.port}`)
 });
 
 
